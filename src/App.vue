6aca804c64c8aaaebd413689da1ae9f8afb04499
Vitalii Demchuk <app.vd@yandex.ru>
2022-08-28 18:47:48 +0300

    upd: styles

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import TodoItem from './components/todo/TodoItem.vue';
+import TodoItem from './components/TodoItem.vue';
 
 export default {
   name: 'App',
@@ -16,16 +16,89 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&display=swap');
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+
+.material-icons {
+  color: #444;
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px; /* Preferred icon size */
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+  -moz-osx-font-smoothing: grayscale;
+  font-feature-settings: 'liga';
+}
+
 body {
+  font-family: 'Open Sans', sans-serif;
   background-color: #eee;
   margin: 0;
 }
 
 .wrapper {
   width: 100%;
-  max-width: 600px;
+  max-width: 300px;
   margin: 12px auto;
   padding: 12px;
   background-color: #fff;
+}
+
+button {
+  margin: 4px;
+  height: 24px;
+  line-height: 24px;
+  padding: 0 6px;
+  border: none;
+  background-color: #eee;
+  border-radius: 5px;
+}
+
+input[type='text'] {
+  height: 18px;
+  line-height: 18px;
+  width: 100%;
+}
+
+.input-checkbox {
+  width: 100%;
+}
+.input-checkbox input[type='checkbox'] {
+  position: absolute;
+  z-index: -1;
+  opacity: 0;
+}
+.input-checkbox input[type='checkbox'] + label {
+  display: inline-flex;
+  align-items: center;
+  user-select: none;
+  width: 100%;
+}
+.input-checkbox input[type='checkbox'] + label::before {
+  content: '';
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  flex-shrink: 0;
+  flex-grow: 0;
+  border: 1px solid #adb5bd;
+  border-radius: 0.25em;
+  margin-right: 0.5em;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 50% 50%;
+}
+.input-checkbox input[type='checkbox']:checked + label::before {
+  border-color: #0b76ef;
+  background-color: #0b76ef;
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <h2>{{ todo.name }}</h2>
+
+    <div v-if="all" class="row row--inline">
+      <span v-if="all">Выполнено: {{ completed }}/{{ all }}</span>
+      <span
+        v-if="all === completed"
+        class="material-icons"
+        :style="{ color: 'green' }"
+      >
+        check
+      </span>
+    </div>
+
+    <div class="row">
+      <input v-model="form.name" type="text" @keyup.enter="add(form)" />
+      <button :disabled="!form.name" @click="add(form)">
+        <span class="material-icons">add</span>
+      </button>
+    </div>
+
+    <div v-for="(item, idx) of todo.items" class="row" :key="item.id">
+      <div class="input-checkbox">
+        <input
+          type="checkbox"
+          :checked="todo.items[idx].check"
+          :id="`check-${idx}`"
+          @change="(e) => onCheckItem(idx, e.target.checked)"
+        />
+        <label
+          :for="`check-${idx}`"
+          :class="{ checked: todo.items[idx].check }"
+        >
+          {{ todo.items[idx].name }}
+        </label>
+      </div>
+
+      <button @click="remove(idx)">
+        <span class="material-icons">clear</span>
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+import { db } from '@/firebase';
+import { ref, set, onValue } from 'firebase/database';
+
+const DEFAULT_ITEM = {
+  check: false,
+  name: null,
+};
+
+export default {
+  data() {
+    return {
+      form: { ...DEFAULT_ITEM },
+      todo: {
+        name: 'Список дел',
+        items: [],
+      },
+    };
+  },
+
+  computed: {
+    all() {
+      return this.todo?.items?.length;
+    },
+    completed() {
+      return this.todo?.items?.filter((el) => el.check).length;
+    },
+  },
+
+  watch: {
+    todo: {
+      deep: true,
+      handler(v) {
+        set(ref(db, 'todo'), v);
+      },
+    },
+  },
+
+  created() {
+    this.registerWatchers();
+  },
+
+  methods: {
+    registerWatchers() {
+      const todo = ref(db, 'todo');
+      onValue(todo, (snapshot) => {
+        this.todo = snapshot.val();
+      });
+    },
+
+    add(form) {
+      if (!this.todo.items) {
+        this.todo.items = [];
+      }
+      this.todo.items.unshift({
+        ...form,
+        id: +new Date(),
+      });
+      this.form = { ...DEFAULT_ITEM };
+    },
+
+    remove(idx) {
+      this.todo.items.splice(idx, 1);
+    },
+
+    onCheckItem(idx, checked) {
+      const items = [...this.todo.items];
+      items[idx].check = checked;
+
+      this.todo.items = items.sort((a, b) => Number(a.check) - Number(b.check));
+    },
+  },
+};
+</script>
+
+<style scoped>
+.row {
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin: 8px 0;
+}
+.row--inline {
+  justify-content: start;
+}
+.checked {
+  text-decoration: line-through;
+}
+</style>
