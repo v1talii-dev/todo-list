42f17a4899782c973cb279fda93f6131d8198de7
Vitalii Demchuk <app.vd@yandex.ru>
2022-08-28 19:14:54 +0300

    upd: settings

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,11 @@ module.exports = defineConfig({
     process.env.NODE_ENV === 'production'
       ? process.env.VUE_APP_PUBLIC_PATH
       : '/',
+  pwa: {
+    name: 'Todo list',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
 });
